c8bad4d7765439496a3f05b51c5cacc698fe7be1
DavidAraB <aa21.dara84@iespabloserrano.com>
2022-01-24 11:07:51 +0100

    asf

--- Tema6/src/Tema6/Ejer03.java
+++ Tema6/src/Tema6/Ejer03.java
@@ -1,9 +1,23 @@
 package Tema6;
 
+import java.util.Arrays;
+
 public class Ejer03 {
+	
+	public void Imp(int[]vector) {
+		Arrays.sort(vector);
+		System.out.println("el max es : "+vector[vector.length-1]+" el min es: "+vector[0]);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Ejer03 imp = new Ejer03();
+		int [] vector = new int [10];
+		for(int i = 0;i<vector.length;++i) {
+			vector[i]= (int)(Math.random()*10)+1;
+			System.out.println(vector[i]);
+		}
+		imp.Imp(vector);
+		
 
 	}
 
